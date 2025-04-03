44e86e18177070ea9ce896d6e1eb1320d5c1f554
Karthikeya Arikirevula <karthikeya.arikirevula@student.tdsb.on.ca>
2025-04-03 03:09:09 +0000

    z

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,47 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+	}
+	
+	public static void selectionSortName(String[] names, int[] ages) {
+		for(int i = 0; i < names.length - 1; i++) {
+			int smallIndex = i;
+			for(int j = i + 1; j < names.length; j++) {
+				
+				
+				if(names[j].toLowerCase().compareTo(names[smallIndex].toLowerCase()) < 0) {
+					smallIndex = j;
+				}
+			}
+			
+			String name = names[i];
+			int age = ages[i];
+			names[i] = names[smallIndex];
+			ages[i] = ages[smallIndex];
 
+			names[smallIndex] = name;
+			ages[smallIndex] = age;
+		}
 	}
 
-	
+	public static void selectionSortAge(String[] names, int[] ages) {
+		for(int i = 0; i < ages.length - 1; i++) {
+			int smallIndex = i;
+			for(int j = i + 1; j < ages.length; j++) {
+				
+				
+				if(ages[j] < ages[smallIndex]) {
+					smallIndex = j;
+				}
+			}
+			
+			String name = names[i];
+			int age = ages[i];
+			names[i] = names[smallIndex];
+			ages[i] = ages[smallIndex];
 
+			names[smallIndex] = name;
+			ages[smallIndex] = age;
+		}
+	}
 }
